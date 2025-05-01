56d1e9cb8a0e92d43934e37bff142a5cc465d845
matthezassistent <m.lunenburg@gmail.com>
2025-05-01 09:27:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -341,7 +341,6 @@ def main():
     app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
-        webhook_path=f"/{WEBHOOK_SECRET}",  
         webhook_url=f"{RENDER_URL}/{WEBHOOK_SECRET}"
     )
    
